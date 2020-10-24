8fdb525c9e32e1e9001872a223913aed9e35397f
Koirul <koirul.2593@gmail.com>
2020-10-24 20:04:46 +0700

    rest api dengan database

--- json/latihan1.php
+++ json/latihan1.php
@@ -1,16 +1,21 @@
 <?php
 
-$mahasiswa= [
-    [
-        "nama"  => "Koirul",
-         "nim"   => "3125111205",
-         "email" => "koirul.2593@gmail.com"
-    ],[
-    "nama"  => "panji",
-    "nim"   => "3125111206",
-    "email" => "koirul.11@gmail.com"
-    ]
-];
+// $mahasiswa= [
+//     [
+//         "nama"  => "Koirul",
+//          "nim"   => "3125111205",
+//          "email" => "koirul.2593@gmail.com"
+//     ],[
+//     "nama"  => "panji",
+//     "nim"   => "3125111206",
+//     "email" => "koirul.11@gmail.com"
+//     ]
+// ];
+
+$dbh = new PDO('mysql:host=localhost;dbname=phpdasar', 'root', '');
+$db =$dbh->prepare('SELECT * FROM mahasiswa');
+$db->execute();
+$mahasiswa = $db->fetchAll(PDO::FETCH_ASSOC);
 
 $data=json_encode($mahasiswa);
 echo $data;
---
+++ json/phpdasar.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 24 Okt 2020 pada 15.03
+-- Versi server: 10.4.14-MariaDB
+-- Versi PHP: 7.2.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `phpdasar`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `nrp` char(9) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `jurusan` varchar(100) NOT NULL,
+  `gambar` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`id`, `nama`, `nrp`, `email`, `jurusan`, `gambar`) VALUES
+(11, '64737', 'Diana', 'd1anasite04@gmail.com', 'teknik informatika ', 'hardik.png'),
+(12, '43757', 'lol', 'siapa@gmail.com', 'teknik informatika', 'rohit.png'),
+(13, 'Koirul', '312511120', 'khoirul.2593@gmail.com', 'Teknik Informatika', 'koirul.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
