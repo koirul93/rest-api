e2476916c60c5f1dc07b85817dedaf1703451ffc
Koirul <koirul.2593@gmail.com>
2020-11-15 15:52:06 +0700

    coba vanila javasrcrib dan jquery

--- json/script.js
+++ json/script.js
@@ -1,6 +1,25 @@
-let mahasiswa = {
-    nama: "koirul",
-    nim: "312511205",
-    email: "koirul.2593@gmail.com"
-}
-console.log(JSON.stringify(mahasiswa));
+//let mahasiswa = {
+ //   nama: "koirul",
+  //  nim: "312511205",
+   // email: "koirul.2593@gmail.com"
+//}
+//console.log(JSON.stringify(mahasiswa));
+
+
+vasina javascibd
+//let xhr = new XMLHttpRequest();
+//xhr.onreadystatechange = function(){
+  //  if (xhr.readyState == 4 && xhr.status ==200) {
+   //     let mahasiswa = JSON.parse(this.responseText);
+   //     console.log(mahasiswa);
+    //}
+//}
+
+//xhr.open('GET', 'coba.json', true);
+//xhr.send();
+
+jquery
+$.GetJSON('coba.json', function (data) {
+    console.log(data);
+
+});
