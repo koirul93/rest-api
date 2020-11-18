9ff29503d0f6e1601d17879e81b1da2e3d9d1a25
Koirul <koirul.2593@gmail.com>
2020-11-18 20:16:01 +0700

    menampilkan data json ke web dengan php

--- hut/latihan1.php
+++ hut/latihan1.php
@@ -47,18 +47,20 @@ echo $menu [0] ["nama"];
       </div>
     </div> 
     <div class="row">
+
+      <?php foreach ($menu as $row) : ?>
       <div class="col-md-4">
-        <div class="card" style="width: 18rem;">
-          <img src="img/menu/american-favourite.jpg" class="card-img-top">
+        <div class="card mb-3">
+          <img src="img/menu/<?= $row["gambar"]; ?>" class="card-img-top">
           <div class="card-body">
-            <h5 class="card-title">American favorite</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-              content.</p>
-              <h5 class="card-title">Rp. 60.000,- </h5>
+            <h5 class="card-title"><?= $row["nama"]; ?></h5>
+            <p class="card-text"><?= $row["deskripsi"]; ?></p>
+              <h5 class="card-title">Rp. <?= $row["harga"]; ?></h5>
             <a href="#" class="btn btn-primary">Pesan Sekarang</a>
           </div>
         </div>
       </div>
+      <?php endforeach; ?>
 
     </div>
 
